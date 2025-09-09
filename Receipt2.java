57c4cf2ba183f3e40ed8cef663d7864cbb98d046
ayaannowaz <ayaannowaz@billericak12.com>
2025-09-09 09:20:12 -0400

    Receipt2BEANZ

---
+++ Receipt2.java
@@ -0,0 +1,31 @@
+/**
+ * Receipt2.java  
+ *
+ * @author – Ayaan Nowaz
+ * @author – 2B
+ */ 
+public class Receipt2
+{
+   public static void main(String [] args) {
+   String name = "Billerica Memorial High School";
+   double drink = 1.50;
+   double candy = 1.25;
+   double hotdog =2.75;
+   double hamburger = 3.50;
+   {
+      System.out.println("*************************************");
+      System.out.println("*                                   *");
+      System.out.println("*  " + name + "   *");
+      System.out.println("*                                   *");
+      System.out.println("*     Drink ..........$" + String.format("%.2f", drink) + "         *");                      
+      System.out.println("*     Candy ..........$" + String.format("%.2f", candy) + "         *");     
+      System.out.println("*     Hot Dog ........$" + String.format("%.2f", hotdog) + "         *");    
+      System.out.println("*     Hamburger ......$" + String.format("%.2f", hamburger) + "         *");     
+      System.out.println("*                                   *");    
+      System.out.println("*************************************");
+   }
+   }
+   }
+
+
+
