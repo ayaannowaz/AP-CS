9ceebf91ae8fc2487667a6e814a68d00e1568c9e
ANowaz <ayaannowaz@billericak12.com>
2025-09-29 21:03:31 -0400

    comlpex

--- Complex.java
+++ Complex.java
@@ -11,7 +11,7 @@ public class Complex {
     imag = 0.0;
     }
     public String toString() {
-        return real + imag + "i";
+        return real + " + " + imag + "i";
     }
     public Complex add(Complex other) {
         double r = this.real + other.real;
@@ -19,14 +19,40 @@ public class Complex {
         Complex n1 = new Complex(r,i);
         return n1;
     }
+    public Complex add(double x) {
+        double r = real + x;
+        double i = imag;
+        Complex n2 = new Complex(r,i);
+        return n2;
+    }
+
     public Complex multiply(Complex other) {
         double r = this.real*other.real - this.imag*other.imag;
         double i = this.real*other.imag + other.real*this.imag;
         return new Complex(r,i);
     }
-
+    public Complex multiply(double y) {
+        double r = real*y;
+        double i = imag*y;
+        Complex n3 = new Complex(r,i);
+        return n3;
+    } 
     public static void main(String[] args) {
         Complex num1 = new Complex (1,2);
         Complex num2 = new Complex (2,3);
+        Complex num3 = new Complex (0, 3);
+        Complex num4 = new Complex (3,0);
+        Complex num5 = new Complex (7,-9);
+        Complex num6 = new Complex (-4,8);
+        
+        Complex apple = num1.add(num2);
+        Complex banana = num3.multiply(num4);
+        Complex cantaloupe = num5.add(7);
+        Complex durian = num6.multiply(-4);
+
+        System.out.println(apple);
+        System.out.println(banana);
+        System.out.println(cantaloupe); 
+        System.out.println(durian);
     }
 }
